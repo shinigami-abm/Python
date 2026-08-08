3e477adc78e0acd20cad84362f677e4ce2aac5a3
abdelmoise <abdelmoise1@gmail.com>
2026-08-09 00:37:34 +0100

    Add files via upload

--- random.py/countor.py
+++ random.py/countor.py
@@ -2,14 +2,20 @@ import numpy as np
 import cv2 as cv
 
 img = cv.imread("fih2.jpeg")
+fih = cv.imread("fih1.jpeg")
+
 temp = img.copy()
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray2 = cv.cvtColor(fih, cv.COLOR_BGR2GRAY)
 #thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, 2)
 ret, thresh = cv.threshold(gray, 127, 255, 0)
+ret, thr = cv.threshold(gray2, 200, 255, 0)
 
 countor, hi = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-#cv.drawContours(img, countor, -1,0, 10)
+co , h = cv.findContours(thr, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(img, countor, -1,0, 10)
 
+#print(cv.matchShapes(countor, co, 0, 0))
 
 #m = cv.moments(countor[20])
 #cx = int(m['m10'] / m['m00'])
@@ -21,7 +27,8 @@ countor, hi = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 #test = cv.bitwise_and(temp, temp , img)
 
-#print(len(countor))
+#print(hi)
+print(len(countor))
 
 #apox = cv.approxPolyDP(countor[20], 0.02*cv.arcLength(countor[0], True), True)
 
@@ -40,7 +47,7 @@ countor, hi = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 #(x,y), r=cv.minEnclosingCircle(countor[5])
 #cv.circle(img,(int(x), int(y)), int(r), 0, 4)
 
-
+#print(cv.pointPolygonTest(countor[5], (276,166), True))
 
 
 
